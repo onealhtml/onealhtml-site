3ac153335d3a841f5974c53f35c8fcf9885ae6a5
Lorenzo Farias <lorenzofarias8@hotmail.com>
2025-03-29 01:44:51 -0300

    Phone fix

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 var canvas = document.querySelector('canvas'),
     ctx = canvas.getContext('2d');
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+function setupCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    fontSize = window.innerWidth <= 480 ? 7 : 10;
+    columns = canvas.width / fontSize;
+
+    drops = [];
+    for (var i = 0; i < columns; i++) {
+        drops[i] = 1;
+    }
+}
 
 var letters = 'ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ';
 letters = letters.split('');
 
-var fontSize = 10,
-    columns = canvas.width / fontSize;
+var fontSize, columns, drops;
 
-var drops = [];
-for (var i = 0; i < columns; i++) {
-    drops[i] = 1;
-}
+setupCanvas();
 
 function draw() {
     ctx.fillStyle = 'rgba(0, 0, 0, .1)';
@@ -31,13 +37,11 @@ function draw() {
 
 setInterval(draw, 33);
 
-// Adicione este evento para redimensionar o canvas quando a janela for redimensionada
-window.addEventListener('resize', function() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    columns = canvas.width / fontSize;
-    drops = [];
-    for (var i = 0; i < columns; i++) {
-        drops[i] = 1;
-    }
-});
+window.addEventListener('resize', setupCanvas);
+
+if (!document.querySelector('meta[name="viewport"]')) {
+    var meta = document.createElement('meta');
+    meta.name = 'viewport';
+    meta.content = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no';
+    document.getElementsByTagName('head')[0].appendChild(meta);
+}
