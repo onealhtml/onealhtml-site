ee66cde851c96828085fe55dab518d1ac558d2e8
Lorenzo Farias <lorenzofarias8@hotmail.com>
2025-12-01 17:05:45 -0300

    Update SoundCloudEmbed track URL to new song

--- src/components/SoundCloudEmbed.tsx
+++ src/components/SoundCloudEmbed.tsx
@@ -22,7 +22,7 @@ export const SoundCloudEmbed = memo(function SoundCloudEmbed() {
         width="100%"
         height="166"
         allow="autoplay"
-        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A2112413862&color=%230f0&auto_play=false&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+        src="https://w.soundcloud.com/player/?url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F2212903193&color=%230f0&auto_play=false&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
         title="SoundCloud player - agua track by onealhtml"
         style={{ border: 'none', display: isLoaded ? 'block' : 'none' }}
         onLoad={() => setIsLoaded(true)}
